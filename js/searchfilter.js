b673fa2d1a1ff372d4f13a02f0dc3fd3148f5c3f
William Ma <williamma2020@icloud.com>
2021-04-28 09:06:55 -0700

    filter issue

---
+++ js/searchfilter.js
@@ -0,0 +1,21 @@
+const search = document.querySelector('#search');
+const boxTexts = document.getElementsByClassName('.name');
+const handleSearch = event => {
+    //define searchTerm as event
+    const searchTerm = event.target.value.toLowerCase();
+    boxTexts.forEach(boxText => {
+        const text = boxText.textContent.toLowerCase();
+        const box = boxText.parentElement;
+
+
+        //if input letter included in the searchTerm, box show up.
+        if(text.includes(searchTerm)) {
+          box.style.display = "block";
+          //otherwise, box disappear
+        } else {
+          box.style.display = "none";  
+        }
+      });
+}
+search.addEventListener('keyup', handleSearch);
+
